d1b3d7ab66aee288a5744e6ebeffe844825d3fe6
Rocky Dey <coderrocky25@gmail.com>
2024-02-26 00:36:37 +0600

    add toast to menu section

--- scripts/resturant-script.js
+++ scripts/resturant-script.js
@@ -9,3 +9,19 @@ console.log(addCarts);
 for (addCart of addCarts) {
   addCart.addEventListener("click", increaseCart);
 }
+
+let myNotify;
+
+function pushNotify() {
+  myNotify = new Notify({
+    status: "success",
+    title: "Successfully Add to Cart",
+    text: "Please check cart button on navbar!",
+    effect: "slide",
+    type: "filled",
+  });
+}
+
+function close() {
+  myNotify.close();
+}
